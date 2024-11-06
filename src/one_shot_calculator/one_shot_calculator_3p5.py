2736af1853007d235ec6ce71e8642676867f753e
Matt von Hippel <mattvonhippel@gmail.com>
2024-11-06 17:48:19 +0100

    more caching using switch to frozendict, updated version number

--- src/one_shot_calculator/one_shot_calculator_3p5.py
+++ src/one_shot_calculator/one_shot_calculator_3p5.py
@@ -3,6 +3,7 @@ Module for calculating the chance to one-shot monsters in D&D 3.5.
 """
 
 import pandas as pd
+from frozendict import frozendict
 from one_shot_calculator.discrete_dists import *
 
 def attack_dist(attack_bonus,armor_class,damage_dist,crit_range=(20,20),crit_mult=2,confirm_bonus=0,crit_effect={0:1}):
@@ -17,8 +18,8 @@ def attack_dist(attack_bonus,armor_class,damage_dist,crit_range=(20,20),crit_mul
     hit_chance=prob_between(basic_die_dist(20),min(max(2,armor_class-attack_bonus),20),20)
     confirm_chance=prob_between(basic_die_dist(20),min(max(2,armor_class-attack_bonus-confirm_bonus),20),20)
     crit_chance=prob_between(basic_die_dist(20),min(max(crit_range[0],armor_class-attack_bonus),20),20)*confirm_chance
-    zerodist={0: prob_between(basic_die_dist(20),1,min(max(1,armor_class-attack_bonus-1),20))}
-    nonzerodist={i: (hit_chance-crit_chance)*prob_get(min_one(damage_dist),i)+crit_chance*prob_get(min_one(add_dists(multiple_dist(crit_mult,damage_dist),crit_effect)),i) for i in range(min_dist(min_one(damage_dist)),max_dist(min_one(add_dists(multiple_dist(crit_mult,damage_dist),crit_effect)))+1)}
+    zerodist=frozendict({0: prob_between(basic_die_dist(20),1,min(max(1,armor_class-attack_bonus-1),20))})
+    nonzerodist=frozendict({i: (hit_chance-crit_chance)*prob_get(min_one(damage_dist),i)+crit_chance*prob_get(min_one(add_dists(multiple_dist(crit_mult,damage_dist),crit_effect)),i) for i in range(min_dist(min_one(damage_dist)),max_dist(min_one(add_dists(multiple_dist(crit_mult,damage_dist),crit_effect)))+1)})
     return nonzerodist | zerodist
 
 def save_chance(save_bonus,difficulty_class):
@@ -28,7 +29,7 @@ def save_chance(save_bonus,difficulty_class):
 def save_for_half_dist(save_bonus,difficulty_class,damage_dist):
     """Returns the probability distribution for damage if one takes half damage on a successful save.
        Rolls with save_bonus against difficulty_class, on a failed save takes damage_dist"""
-    return {i: save_chance(save_bonus,difficulty_class)*prob_get(min_one(mult_dist_by_const(min_one(damage_dist),0.5)),i)+(1-save_chance(save_bonus,difficulty_class))*prob_get(min_one(damage_dist),i) for i in range(max(1,int(0.5*min_dist(damage_dist)//1)),max_dist(damage_dist)+1)}
+    return frozendict({i: save_chance(save_bonus,difficulty_class)*prob_get(min_one(mult_dist_by_const(min_one(damage_dist),0.5)),i)+(1-save_chance(save_bonus,difficulty_class))*prob_get(min_one(damage_dist),i) for i in range(max(1,int(0.5*min_dist(damage_dist)//1)),max_dist(damage_dist)+1)})
 
 def one_shot_histogram(dataframe,low_CR,high_CR,one_shot_function):
     """Returns a histogram of the chance that an attack one-shots monsters in a given CR range.
